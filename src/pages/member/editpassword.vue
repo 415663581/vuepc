f67c7fab415d89e98b6c87f782c5df5de047aeb0
jie <1174787239@qq.com>
2019-10-23 17:34:18 +0800

    密码修改

--- src/pages/member/editpassword.vue
+++ src/pages/member/editpassword.vue
@@ -2,121 +2,96 @@
 	<div class="container-page">
 		<el-breadcrumb separator="/">
         <el-breadcrumb-item to="/member">会员中心</el-breadcrumb-item>
-        <el-breadcrumb-item>我的购物车</el-breadcrumb-item>
+        <el-breadcrumb-item>修改密码</el-breadcrumb-item>
         </el-breadcrumb>
 		<el-row>
-			<el-form>
+			<el-form :rules="rules" :model="form" ref="edit" label-width="90px">
 				<el-row>
-					<el-col :span='10'>
-						<el-form-item label="用户姓名:" label-width="90px" >
-							<span>{{userData.username}}</span>
-						</el-form-item>
-					</el-col>
-				</el-row>
-				<el-row>
-					<el-col :span='10'>
-						<el-form-item label="用户电话:" label-width="90px" >
-							<span>{{userData.telphone}}</span>
-							</el-form-item>
-					</el-col>
-				</el-row>
-				<el-row>
-					<el-col :span='10'>
-						<el-form-item label="新密码:" label-width="90px" >
-							<el-input v-model='userData.gender' v-on:blur="getuse('gender')"></el-input>
-						</el-form-item>
-					</el-col>
-				</el-row>
-				<el-row>
-					<el-col :span='10'>
-						<el-form-item label="确认密码:" label-width="90px" >
-							<el-cascader
-								v-model="value"
-								:options="options"
-								@change="handleChange"></el-cascader>
-							</el-form-item>
-					</el-col>
-				</el-row>
+					<el-form-item label="用户姓名:">
+						<span>{{userData.username}}</span>
+					</el-form-item>
+					<el-form-item label="用户电话:">
+						<span>{{userData.telphone}}</span>
+					</el-form-item>
+					<el-form-item label="新密码:"  prop="password" >
+						<el-input type="password" v-model="form.password"></el-input>
+					</el-form-item>
+					<el-form-item label="确认密码:" prop="repassword">
+						<el-input type="password" v-model="form.repassword"></el-input>
+					</el-form-item>
+					<el-form-item>
+						<el-button type="primary" @click="editPassword">确认</el-button>
+						<el-button>取消</el-button>
+					</el-form-item>
+				</el-row>   
 			</el-form>
 		</el-row>
 	</div>
 </template>
 
 <script>
-import City from '@/utils/city'
 export default {
-		name: 'Userinfo',
-		data(){
-			return {
-			    options: City,
-			    fullscreenLoading: false,
-				userData: this.$store.state.userInfo
+	// name: 'Userinfo',
+	data(){
+		return {
+			fullscreenLoading: false,
+			userData: this.$store.state.userInfo,
+			form: {
+				password: '',
+				repassword: ''
+			},
+			rules: {
+				password: [
+					{required: true, message: '请输入新密码', trigger: 'blur'},
+					{min: 5, max: 20, message: '密码长度在 5 到 20个字符', trigger: 'blur'}
+				],
+				repassword: [
+					{required: true, message: '请输入密码', trigger: 'blur'},
+					{validate(rules, value, callback) {
+						if (value != this.form.password) {
+							callback(new Error('确认密码不正确'))
+						}
+					},
+					trigger: 'blur'
+					}
+				]
 			}
-		},
-		methods:{
-			   handleAvatarSuccess(res, file) {
-			    this.fullscreenLoading = false;
-		        this.userData.avatarUrl = res.data.src;
-		        this.getuse('avatarUrl');
-		      },
-		      beforeAvatarUpload(file) {
-                this.fullscreenLoading = true;
-		        const isJPG = file.type === 'image/jpeg';
-		        const isLt2M = file.size / 1024 / 1024 < 2;
-
-		        if (!isJPG) {
-		          this.$message.error('上传头像图片只能是 JPG 格式!');
-		        }
-		        if (!isLt2M) {
-		          this.$message.error('上传头像图片大小不能超过 2MB!');
-		        }
-		        return isJPG && isLt2M;
-		      },
-		      getuse(key){
-		        if (key && this['userData'][key] != '') {
-                    let id = this.$store.state.user.userData.id
-                    this.axios.post("/api/User/editUserInfo",{
-                    params:{
-                            accountid:id,
-                            [key]:this['userData'][key]
-                        }
-                    })
-                    .then(function(response){
-                        if(response.data.code == 0){
-                            this.$store.commit('user/updateUserInfo',{[key]: this[key]});
-                        }
-                    }.bind(this))
-                     .catch(function (error) {
-                        this.$message('服务器繁忙，请稍后重试');
-                    }.bind(this));
-
-                }
-		      },
-              goBack() {
-                 this.$router.push('/member');
-              },
-		},
-		created(){
-			let use = this.$store.state.user.userData;
-			this.name=use.username;
-			this.telphone=use.telphone;
-			this.nickName= use.nickName;
-			this.city = use.city;
-			this.gender = use.gender;
-			this.imageUrl = use.avatarUrl;
-			console.log(use.city)
-		},
-		actived(){
-
+		}
+	},
+	methods:{
+		editPassword () {  
+			this.$refs.edit.validate((status) => {
+				if (status) {
+					this.axios.post('/api/user/edit', {
+						params: {
+							id: this.userData.id,
+							username: this.userData.username,
+							telphone: this.userData.telphone,
+							authority: this.userData.authority,
+							password: this.form.password
+						}
+					}).then((data) => {
+						if (data.data.code == 0) {
+							this.$alert('修改成功', '提示')
+							this.$router.go(-1)
+						} else {
+							this.$alert(data.data.msg, '提示')
+						}
+					}).catch((error) => {
+						this.$alert('网络错误请稍后重试', '提示')
+					})
+				}
+			})
 		}
 	}
+}
 </script>
 
 <style scoped="scoped">
 	.form{
 		margin: 0px auto;
 	}
-	 .avatar-uploader .el-upload {
+	.avatar-uploader .el-upload {
     border: 1px dashed #eee;
     border-radius: 6px;
     cursor: pointer;
